12482376fad0040e178cc35096e98e64ce9d4040
Andrea Russo <andrea.russo@cmcc.it>
2018-12-01 16:27:02 +0100

    Completed the scramble function.

--- src/scramblies/scramble.clj
+++ src/scramblies/scramble.clj
@@ -8,14 +8,15 @@
       (> str2-length str1-length) false
       (= str1-length 0) false
       :else
-      (let [str2-freq-map (frequencies str2)]
-        (if (first
-             (drop-while (fn [m]
-                           (some #(> (val %) (get m (key %) 0))
-                                 str2-freq-map))
-                         (reductions (fn [counts x]
-                                       (assoc counts x (inc (get counts x 0))))
-                                     {}
-                                     str1)))
+      (let [str2-freq-map (frequencies str2)
+            freq-map-count (count str2-freq-map)]
+        (if (->> (reductions (fn [counts x]
+                               (assoc counts x (inc (get counts x 0))))
+                             {} str1)
+                 (drop-while (fn [m] (< (count m) freq-map-count)))
+                 (drop-while (fn [m]
+                               (some #(> (val %) (get m (key %) 0))
+                                     str2-freq-map)))
+                 first)
           true
           false)))))
