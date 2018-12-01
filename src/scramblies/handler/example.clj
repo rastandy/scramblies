c6b982325b50b3fb3e30c464976832a2b6474483
Andrea Russo <andrea.russo@cmcc.it>
2018-12-01 16:32:40 +0100

    Removed the example handler and test case.

--- src/scramblies/handler/example.clj
+++
@@ -1,8 +0,0 @@
-(ns scramblies.handler.example
-  (:require [compojure.core :refer :all]
-            [integrant.core :as ig]))
-
-(defmethod ig/init-key :scramblies.handler/example [_ options]
-  (context "/example" []
-    (GET "/" []
-      {:body {:example "data"}})))
